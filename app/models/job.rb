61a3eb31fbb10298c8006d12f19c155fc4320153
AutumnIDavis <foreverautumn16@outlook.com>
2018-05-17 15:13:09 -0400

    Description  validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,7 @@ class Job < ApplicationRecord
 # has_and_belongs_to_many :boats
 belongs_to :user
 validates :cost, numericality: { greater_than_or_equal_to: 1000 }
+validates :description, length: { minimum: 50, too_short: "Minimum of %{count} characters required for description." }
 validates_uniqueness_of :name
 
 end
